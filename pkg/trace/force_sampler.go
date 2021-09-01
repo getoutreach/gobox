d0f4a51cb2f29b73d20dc1c13363480a17f8e2ea
George Shaw <george.e.shaw.iv@outlook.com>
2021-08-31 17:14:28 -0700

    feat: Force Sample Rate for Tracing [DTSS-721] (#48)
    * Allow traces to force their sample rates.
    Co-authored-by: Ramesh (VK) <52936074+vk-outreach@users.noreply.github.com>

--- pkg/trace/force_sampler.go
+++ pkg/trace/force_sampler.go
@@ -10,11 +10,13 @@ import (
 type samplerHook func(map[string]interface{}) (bool, int)
 
 const (
-	fieldTraceID    = "trace.trace_id"
-	fieldForceTrace = "force_trace"
+	fieldTraceID     = "trace.trace_id"
+	fieldForceTrace  = "force_trace"
+	fieldSampleTrace = "sample_trace"
 )
 
 // forceSampler allows force sample rate to 100% when trace context contains field force_trace
+// and sample at a rate of 1/<given rate> when context contains field sample_trace.
 func forceSampler(sampleRate uint) samplerHook {
 	sampler, err := sample.NewDeterministicSampler(sampleRate)
 	if err != nil {
@@ -25,6 +27,13 @@ func forceSampler(sampleRate uint) samplerHook {
 		if _, ok := fields[fieldForceTrace]; ok {
 			return true, 1
 		}
+
+		if rawRate, ok := fields[fieldSampleTrace]; ok {
+			if rate, ok := rawRate.(uint); ok {
+				return true, int(rate)
+			}
+		}
+
 		if traceID, ok := fields[fieldTraceID].(string); ok {
 			return sampler.Sample(traceID), sampler.GetSampleRate()
 		}
@@ -35,6 +44,16 @@ func forceSampler(sampleRate uint) samplerHook {
 func forceTracing(ctx context.Context) context.Context {
 	if t := trace.GetTraceFromContext(ctx); t != nil {
 		t.AddField(fieldForceTrace, "true")
+	}
+	return ctx
+}
+
+// sampleAt sets the sample rate for a given trace explicitly.
+// The rate specifies how many samples were looked at for each
+// accepted sample.  That is, the actual sampling  = 1/rate.
+func sampleAt(ctx context.Context, rate uint) context.Context {
+	if t := trace.GetTraceFromContext(ctx); t != nil {
+		t.AddField(fieldSampleTrace, rate)
 	}
 	return ctx
 }
