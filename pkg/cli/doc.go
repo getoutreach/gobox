054250d61a5aa393401b60f27b22fee2106ecefe
Jared Allard <jaredallard@users.noreply.github.com>
2021-11-04 15:50:42 -0700

    Update pkg/cli/doc.go
    Co-authored-by: George Shaw <george.e.shaw.iv@outlook.com>

--- pkg/cli/doc.go
+++ pkg/cli/doc.go
@@ -1,3 +1,7 @@
+// Copyright 2021 Outreach Corporation. All Rights Reserved.
+
+// Description: File that houses the package documentation for the cli package.
+
 // Package cli contains various cli utilities that are useful for building
 // cli applications with gobox.
 package cli
