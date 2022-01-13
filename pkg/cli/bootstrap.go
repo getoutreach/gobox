7e906049e9db8b168dcc9891e1ddbd915c63c547
Jared Allard <jaredallard@users.noreply.github.com>
2022-01-13 15:46:52 -0800

    fix(cli): dont panic if no fallback reader is set (#113)
    * fix(cli): dont panic if no fallback reader is set
    
    * remove useless whitespace

--- pkg/cli/bootstrap.go
+++ pkg/cli/bootstrap.go
@@ -35,13 +35,24 @@ const UpdateExitCode = 5
 // overrideConfigLoaders fakes certain parts of the config that usually get pulled
 // in via mechanisms that don't make sense to use in CLIs.
 func overrideConfigLoaders(honeycombAPIKey, dataset string, tracingDebug bool) {
+	// override the secret loader so that we can read specific keys from variables
+	// otherwise fallback to the original secret loader, if it was set.
 	var fallbackSecretLookup func(context.Context, string) ([]byte, error)
-	fallbackSecretLookup = secrets.SetDevLookup(func(ctx context.Context, key string) ([]byte, error) {
-		if key == "APIKey" {
+	fallbackSecretLookup = secrets.SetDevLookup(func(ctx context.Context, path string) ([]byte, error) {
+		// use the embedded in value
+		if path == "APIKey" {
 			return []byte(honeycombAPIKey), nil
 		}
 
-		return fallbackSecretLookup(ctx, key)
+		// if no fallback, return an error, failed to find :(
+		// note: as of this time the secrets logic looks for
+		// the path before falling back to the devlookup so this
+		// is safe to assume all attempts have failed
+		if fallbackSecretLookup == nil {
+			return nil, fmt.Errorf("failed to find secret at path '%s', or compiled into binary", path)
+		}
+
+		return fallbackSecretLookup(ctx, path)
 	})
 
 	fallbackConfigReader := cfg.DefaultReader()
