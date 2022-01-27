28ea539b08ac356fe46a6e651a4d190567928a48
Alexandr Maximov <alexandr.maximov@outreach.io>
2022-01-27 04:58:24 +0100

    refactor: unit test

--- pkg/trace/call_option_test.go
+++ pkg/trace/call_option_test.go
@@ -10,34 +10,31 @@ import (
 	"gotest.tools/v3/assert"
 )
 
-func TestWithOptions(t *testing.T) {
-	scheduledAt := time.Now()
-
+func startCall(opt call.Option) *call.Info {
 	var callInfo *call.Info
 	trace.StartCall(
 		context.Background(),
 		"test",
-		trace.WithScheduledTime(scheduledAt),
-		trace.AsGRPCCall(),
 		call.Option(func(c *call.Info) {
 			callInfo = c
 		}),
+		opt,
 	)
+	return callInfo
+}
 
+func TestWithOptions(t *testing.T) {
+	scheduledAt := time.Now()
+	callInfo := startCall(trace.WithScheduledTime(scheduledAt))
 	assert.Equal(t, scheduledAt, callInfo.Times.Scheduled)
+}
+
+func TestAsGRPCCall(t *testing.T) {
+	callInfo := startCall(trace.AsGRPCCall())
 	assert.Equal(t, call.TypeGRPC, callInfo.Type)
 }
 
 func TestAsOutboundCall(t *testing.T) {
-	var callInfo *call.Info
-	trace.StartCall(
-		context.Background(),
-		"test",
-		trace.AsOutboundCall(),
-		call.Option(func(c *call.Info) {
-			callInfo = c
-		}),
-	)
-
+	callInfo := startCall(trace.AsOutboundCall())
 	assert.Equal(t, call.TypeOutbound, callInfo.Type)
 }
