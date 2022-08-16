9261bf22f54b381e2382c01a63ccc9af8324f08d
artemchernyak-outreach <artem.chernyak@outreach.io>
2022-08-16 12:01:04 -0500

    add comments to Otel struct

--- pkg/trace/config.go
+++ pkg/trace/config.go
@@ -21,15 +21,24 @@ func (g *GlobalTags) MarshalLog(addField func(key string, v interface{})) {
 }
 
 type Otel struct {
-	Enabled            bool       `yaml:"Enabled"`
-	Endpoint           string     `yaml:"Endpoint"`
-	AdditionalEndpoint string     `yaml:"AdditionalEndpoint"`
-	Dataset            string     `yaml:"Dataset"`
-	SamplePercent      float64    `yaml:"SamplePercent"`
-	Debug              bool       `yaml:"Debug"`
-	Stdout             bool       `yaml:"Stdout"`
-	APIKey             cfg.Secret `yaml:"APIKey"`
-	AdditionalAPIKey   cfg.Secret `yaml:"AdditionalAPIKey"`
+	// Enabled determines whether to turn on tracing
+	Enabled bool `yaml:"Enabled"`
+	// Endpoint for the tracing backend
+	Endpoint string `yaml:"Endpoint"`
+	// AdditionalEndpoint another backend for tracing
+	AdditionalEndpoint string `yaml:"AdditionalEndpoint"`
+	// Dataset the honeycomb grouping of traces
+	Dataset string `yaml:"Dataset"`
+	// SamplePercent the rate at which to sample
+	SamplePercent float64 `yaml:"SamplePercent"`
+	// Debug allows printing debug statements for traces
+	Debug bool `yaml:"Debug"`
+	// Stdout also outputs traces to stdout
+	Stdout bool `yaml:"Stdout"`
+	// APIKey used for authentication with the backend at Endpoint
+	APIKey cfg.Secret `yaml:"APIKey"`
+	// AdditionalAPIKey used for authentication with the backend at AdditionalEndpoint
+	AdditionalAPIKey cfg.Secret `yaml:"AdditionalAPIKey"`
 }
 
 func (c *Config) Load() error {
