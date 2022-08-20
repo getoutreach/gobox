825be24a0f1c2b7801678b27d8f01058d1467a15
Jared Allard <jared.allard@outreach.io>
2022-08-20 19:30:47 +0200

    archive: make sequencedCloser less likely to panic on nil

--- pkg/updater/archive/io.go
+++ pkg/updater/archive/io.go
@@ -6,20 +6,20 @@ package archive
 
 import "io"
 
-// sequencedReadCloser is a reader that closes all other readers it contains
-// in the order they were added. The first provided reader is embedded
-// and implements the io.ReadCloser, minus the Close() method which is
-// implemented by the sequencedCloser.
+// sequencedReadCloser is a ReadCloser that closes all other readers it
+// contains in the order they were added when Close is called.
+// The first provided reader is embedded and implements the io.ReadCloser,
+// minus the Close() method which is implemented by the sequencedCloser.
 type sequencedReadCloser struct {
 	io.ReadCloser
-	closers []io.ReadCloser
+	rcs []io.ReadCloser
 }
 
-// Close closes all of the contained readers in the order they were added
+// Close closes all of the contained ReadClosers in the order they were added
 // to the sequencedCloser. If a reader fails to close, an error is returned
 // and the rest are NOT closed.
 func (n *sequencedReadCloser) Close() error {
-	for _, c := range n.closers {
+	for _, c := range n.rcs {
 		if err := c.Close(); err != nil {
 			return err
 		}
@@ -29,6 +29,11 @@ func (n *sequencedReadCloser) Close() error {
 }
 
 // newSequencedReadCloser returns a new sequencedReadCloser
-func newSequencedReadCloser(closers ...io.ReadCloser) *sequencedReadCloser {
-	return &sequencedReadCloser{closers[0], closers}
+func newSequencedReadCloser(rcs ...io.ReadCloser) *sequencedReadCloser {
+	// If we were provided no ReadClosers, return nil
+	if len(rcs) == 0 {
+		return nil
+	}
+
+	return &sequencedReadCloser{rcs[0], rcs}
 }
