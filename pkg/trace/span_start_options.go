9d90c320a6e4a42aa941ee5a7827f6d45f53abbc
Nissim Natanov <nissim.natanov@outreach.io>
2022-09-08 10:56:09 -0700

    feat(trace): SpanStartOption and WithLinks

---
+++ pkg/trace/span_start_options.go
@@ -0,0 +1,44 @@
+package trace
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+// SpanStartOption is an interface for various options to be provided
+// during span creation (StartSpan).
+type SpanStartOption interface {
+	// otelOption converts the current option to the native otel's SpanStartOption
+	// it is intentionally unexported to block external implementations of SpanStartOption
+	otelOption(t *otelTracer) trace.SpanStartOption
+}
+
+// WithLink links an external span to the current one. The link can only happen
+// when trace span starts (they cannot be attached mid-span lifetime).
+//
+// The caller should not modify the argument traceHeaders map after calling WithLink.
+//
+// The method accepts trace headers as a map: assumption is current 'context' hosts the parent span
+// and the linked contexts come from 'outside' by other means (e.g. clerk system event headers).
+// In case you need to link trace with a span and you have direct access to that Span's context,
+// you can use trace.ToHeaders to extract the same headers map.
+func WithLink(traceHeaders map[string][]string, name string) SpanStartOption {
+	return linkOption{traceHeaders: traceHeaders, name: name}
+}
+
+// linkOption implements SpanStartOption
+type linkOption struct {
+	// traceHeaders to create span from
+	traceHeaders map[string][]string
+	// name to use for the linked span
+	name string
+}
+
+// otelOption generates a link to be attached to the StartSpan call
+func (o linkOption) otelOption(t *otelTracer) trace.SpanStartOption {
+	// must have fresh context as an input to avoid any external pollution of the span context
+	linkCtx := t.fromHeaders(context.Background(), o.traceHeaders, o.name)
+	link := trace.LinkFromContext(linkCtx)
+	return trace.WithLinks(link)
+}
