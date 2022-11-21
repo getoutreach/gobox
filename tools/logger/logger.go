f0ed1c67d589146675938012b7adaad05bf2b95c
Mukundan Kavanur Kidambi <mukundan.kidambi@outreach.io>
2022-11-21 15:14:25 -0800

    fixing things

--- tools/logger/logger.go
+++ tools/logger/logger.go
@@ -65,7 +65,7 @@ func main() {
 		args = []string{"."}
 	}
 
-	mode := packages.NeedName | packages.NeedTypes | packages.NeedTypesInfo | packages.NeedImports
+	mode := packages.NeedName | packages.NeedTypes | packages.NeedTypesInfo | packages.NeedImports | packages.NeedDeps
 	cfg := &packages.Config{Mode: mode, Tests: false}
 	pkgs, err := packages.Load(cfg, args...)
 	if err != nil || len(pkgs) != 1 {
