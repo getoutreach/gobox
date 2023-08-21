039eb8cfa64a36ab827bd08b3ee635f6609aaa69
Chris Robinson <chris.robinson@outreach.io>
2023-08-21 13:33:39 -0700

    Update pkg/statuscodes/statuscodes.go
    Co-authored-by: Jared Allard <jaredallard@users.noreply.github.com>

--- pkg/statuscodes/statuscodes.go
+++ pkg/statuscodes/statuscodes.go
@@ -48,7 +48,7 @@ const (
 	// Well-behaved services will even return an expected duration for the client to retry-after.
 	RateLimited StatusCode = 705
 	// ClientCanceled is for when the client has canceled the request, and the server has not yet started processing.
-	// It is expectedc that sometimes the client will simply have a canceled request and no server request will have
+	// It is expected that sometimes the client will simply have a canceled request and no server request will have
 	// been made.
 	ClientCanceled StatusCode = 706
 
