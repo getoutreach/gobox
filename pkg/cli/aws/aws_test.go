getoutreach/gobox

commit dddec51f8b570997a49d5e510a2755d01f348f49

feat(aws): remove saml2aws support (#384)
## What this PR does / why we need it

Remove the code to support use of `saml2aws` for AWS credentials as
Outreach no longer uses it, while retaining the refactor to allow future
non-`okta-aws-cli` implementations if necessary.

## Notes for your reviewers

This doesn't completely remove all traces of `saml2aws` as we still use
their AWS credentials reader API to see if the credentials are expired.

pkg/box/box.go
@@ -40,19 +40,8 @@ type AWSConfig struct {
 
 	// RefreshMethod is the CLI used to refresh AWS credentials.
 	// Known values:
-	// * saml2aws (default)
-	// * okta-aws-cli
+	// * okta-aws-cli (default)
 	RefreshMethod string `yaml:"refreshMethod"`
-
-	Okta OktaConfig `yaml:"okta"`
-}
-
-// OktaConfig is Okta-related configuration for AWS authorization.
-type OktaConfig struct {
-	// DefaultMFA is the default MFA type to use when using the
-	// saml2aws refresh method and outputting in the credential
-	// provider format.
-	DefaultMFA string `yaml:"defaultMFA"`
 }
 
 type DeveloperEnvironmentConfig struct {
@@ -152,10 +141,10 @@ type SnapshotConfig struct {
 	// DefaultName is the default name (snapshot) to use, e.g. flagship
 	DefaultName string `yaml:"defaultName"`
 
-	// ReadAWSRole is the role to use, if set, for saml2aws for RO access
+	// ReadAWSRole is the role to use, if set, for RO access to AWS
 	ReadAWSRole string `yaml:"readAWSRole"`
 
-	// WriteAWSRole is the role to use, if set, for saml2aws for RW access
+	// WriteAWSRole is the role to use, if set, for RW access to AWS
 	WriteAWSRole string `yaml:"writeAWSRole"`
 }
 

pkg/cli/aws/aws.go
@@ -108,8 +108,8 @@ func needsRefresh(copts *CredentialOptions) (needsNewCreds bool, reason string) 
 			return true, "No existing credentials"
 		}
 
-		// Attempt to refresh the aws credentials via saml2aws if
-		// they can expire. If they can refresh within 10 minutes of
+		// Attempt to refresh the aws credentials via the credential provider
+		// if they can expire. If they can refresh within 10 minutes of
 		// the expiration period or if they are expired.
 		if time.Now().Add(10 * time.Minute).After(creds.Expires) {
 			return true, "Credentials are expired"
@@ -134,12 +134,8 @@ func AuthorizeCredentials(ctx context.Context, copts *CredentialOptions, acopts 
 		}
 		switch b.AWS.RefreshMethod {
 		case "okta-aws-cli":
-			if err := refreshCredsViaOktaAWSCLI(ctx, copts, acopts, reason); err != nil {
-				return err
-			}
-		case "saml2aws":
 		case "":
-			if err := refreshCredsViaSaml2aws(ctx, copts, acopts, reason); err != nil {
+			if err := refreshCredsViaOktaAWSCLI(ctx, copts, acopts, reason); err != nil {
 				return err
 			}
 		default:
@@ -206,49 +202,6 @@ func refreshCredsViaOktaAWSCLI(ctx context.Context, copts *CredentialOptions, ac
 		err := runCmd(ctx, "okta-aws-cli", args...)
 		if err != nil {
 			return errors.Wrap(err, "failed to refresh AWS credentials via okta-aws-cli")
-		}
-	}
-
-	return nil
-}
-
-// refreshCredsViaSaml2aws refreshes the AWS credentials in the AWS
-// credentials file via the saml2aws CLI tool.
-func refreshCredsViaSaml2aws(ctx context.Context, copts *CredentialOptions, acopts *AuthorizeCredentialsOptions, reason string) error {
-	if !acopts.DryRun {
-		if _, err := exec.LookPath("saml2aws"); err != nil {
-			return fmt.Errorf("failed to find saml2aws, please run orc setup")
-		}
-	}
-
-	if copts.Log != nil {
-		copts.Log.WithField("reason", reason).Info("Obtaining AWS credentials via Okta")
-	}
-
-	args := []string{
-		"login",
-		"--profile",
-		copts.Profile,
-		"--force",
-	}
-
-	if !copts.chooseRoleInteractively() {
-		args = append(args, "--role", copts.Role)
-	}
-
-	if acopts.Output == OutputCredentialProvider {
-		args = append(args, "--credential-process")
-		if acopts.MFA != "" {
-			args = append([]string{"--mfa", acopts.MFA}, args...)
-		}
-	}
-
-	if acopts.DryRun {
-		copts.Log.Infof("Dry Run: saml2aws %s", strings.Join(args, " "))
-	} else {
-		err := runCmd(ctx, "saml2aws", args...)
-		if err != nil {
-			return errors.Wrap(err, "failed to refresh AWS credentials via saml2aws")
 		}
 	}
 

commit 76ff6eab805ae4ef679ca85a3643f1107d78a78b

fix(aws): add support for upstream okta-aws-cli v2 (#391)
## What this PR does / why we need it

Fixes a regression in #384 and adds support for v2 of okta-aws-cli by
checking the output of `okta-aws-cli --version`.

Also updates the call to `okta-aws-cli` to directly set the appropriate
`OKTA_*` environment variables from the box config, instead of relying
on the parent shell environment. This caused problems when the external
dependency that pulled the values from the box config into the shell
environment broke on macOS.

## Jira ID

[DT-4091]

[DT-4091]:
https://outreach-io.atlassian.net/browse/DT-4091?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---------

Co-authored-by: Jared Allard <jaredallard@users.noreply.github.com>

pkg/box/box.go
@@ -42,6 +42,20 @@ type AWSConfig struct {
 	// Known values:
 	// * okta-aws-cli (default)
 	RefreshMethod string `yaml:"refreshMethod"`
+	// Okta contains configuration for using Okta authentication
+	// with AWS.
+	Okta OktaConfig `yaml:"okta"`
+}
+
+// OktaConfig is the Okta-specific config used for AWS access.
+type OktaConfig struct {
+	// FederationAppID is the Okta app ID for the AWS federation app.
+	FederationAppID string `yaml:"federationAppID"`
+	// OIDCClientID is the Okta app ID for the AWS OIDC app (not the
+	// federation one).
+	OIDCClientID string `yaml:"oidcClientID"`
+	// OrgDomain is the hostname of the Okta instance.
+	OrgDomain string `yaml:"orgDomain"`
 }
 
 type DeveloperEnvironmentConfig struct {

pkg/cli/aws/aws.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 	"time"
 
@@ -56,14 +57,25 @@ func (c *CredentialOptions) chooseRoleInteractively() bool {
 	return c.Role == ""
 }
 
+// vRE is the regular expression used to determine the okta-aws-cli
+// version being executed.
+var vRE = regexp.MustCompile(`okta-aws-cli version (\d+)`)
+
 type CredentialsOutput string
 
 // Possible CredentialsOutput values.
 const (
-	// OutputCredentialProvider is the value used to specify that the
-	// CLI used needs to output credential provider compliant JSON.
+	// OutputCredentialProviderV1 is the value used to specify that the
+	// CLI used needs to output credential provider compliant JSON, in
+	// the forked version of okta-aws-cli v1.
 	// nolint: gosec // Why: These aren't credentials.
-	OutputCredentialProvider CredentialsOutput = "credential-provider"
+	OutputCredentialProviderV1 CredentialsOutput = "credential-provider"
+
+	// OutputCredentialProvider is the value used to specify that the
+	// CLI used needs to output credential provider compliant JSON, in
+	// okta-aws-cli v2 and later.
+	// nolint: gosec // Why: These aren't credentials.
+	OutputCredentialProvider CredentialsOutput = "process-credentials"
 )
 
 // AuthorizeCredentialsOptions are optional arguments for the
@@ -133,9 +145,8 @@ func AuthorizeCredentials(ctx context.Context, copts *CredentialOptions, acopts 
 			return errors.Wrap(err, "could not load refresh credential config")
 		}
 		switch b.AWS.RefreshMethod {
-		case "okta-aws-cli":
-		case "":
-			if err := refreshCredsViaOktaAWSCLI(ctx, copts, acopts, reason); err != nil {
+		case "okta-aws-cli", "":
+			if err := refreshCredsViaOktaAWSCLI(ctx, copts, acopts, b, reason); err != nil {
 				return err
 			}
 		default:
@@ -168,10 +179,25 @@ func EnsureValidCredentials(ctx context.Context, copts *CredentialOptions) error
 
 // refreshCredsViaOktaAWSCLI refreshes the AWS credentials in the AWS
 // credentials file via the okta-aws-cli CLI tool.
-func refreshCredsViaOktaAWSCLI(ctx context.Context, copts *CredentialOptions, acopts *AuthorizeCredentialsOptions, reason string) error {
-	if !acopts.DryRun {
+func refreshCredsViaOktaAWSCLI(
+	ctx context.Context,
+	copts *CredentialOptions,
+	acopts *AuthorizeCredentialsOptions,
+	b *box.Config,
+	reason string,
+) error {
+	useCredentialProviderOutput := acopts.Output == OutputCredentialProvider || acopts.Output == OutputCredentialProviderV1
+	cliExists := true
+	if !acopts.DryRun || useCredentialProviderOutput {
 		if _, err := exec.LookPath("okta-aws-cli"); err != nil {
-			return fmt.Errorf("failed to find okta-aws-cli in PATH")
+			if !acopts.DryRun {
+				return fmt.Errorf("failed to find okta-aws-cli in PATH")
+			}
+
+			if copts.Log != nil {
+				copts.Log.Warnln("Cannot find okta-aws-cli in PATH but in dry run mode")
+			}
+			cliExists = false
 		}
 	}
 
@@ -190,8 +216,12 @@ func refreshCredsViaOktaAWSCLI(ctx context.Context, copts *CredentialOptions, ac
 		args = append(args, "--aws-iam-role", copts.Role)
 	}
 
-	if acopts.Output == OutputCredentialProvider {
-		args = append(args, "--format", string(OutputCredentialProvider))
+	if useCredentialProviderOutput {
+		isCLIVersion1, err := isOktaAwsCliVersion1(ctx, cliExists)
+		if err != nil {
+			return errors.Wrap(err, "could not determine okta-aws-cli version")
+		}
+		args = append(args, "--format", string(credentialProviderFormat(isCLIVersion1)))
 	} else {
 		args = append(args, "--write-aws-credentials")
 	}
@@ -199,7 +229,7 @@ func refreshCredsViaOktaAWSCLI(ctx context.Context, copts *CredentialOptions, ac
 	if acopts.DryRun {
 		copts.Log.Infof("Dry Run: okta-aws-cli %s", strings.Join(args, " "))
 	} else {
-		err := runCmd(ctx, "okta-aws-cli", args...)
+		err := runOktaAwsCLI(ctx, b, args...)
 		if err != nil {
 			return errors.Wrap(err, "failed to refresh AWS credentials via okta-aws-cli")
 		}
@@ -208,12 +238,75 @@ func refreshCredsViaOktaAWSCLI(ctx context.Context, copts *CredentialOptions, ac
 	return nil
 }
 
-// runCmd is a wrapper for running a command via exec.CommandContext
-// and passing through stdin/stdout/stderr.
-func runCmd(ctx context.Context, name string, args ...string) error {
-	cmd := exec.CommandContext(ctx, name, args...)
+// oktaAwsCLICmd is a convenience function that creates an exec.Cmd
+// instance for okta-aws-cli.
+func oktaAwsCLICmd(ctx context.Context, args ...string) *exec.Cmd {
+	cmd := exec.CommandContext(ctx, "okta-aws-cli", args...)
 	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
+	return cmd
+}
+
+// runOktaAwsCLI is a wrapper for running okta-aws-cli via exec.CommandContext,
+// passing through stdin/stdout/stderr, and setting the appropriate
+// environment variables.
+func runOktaAwsCLI(ctx context.Context, b *box.Config, args ...string) error {
+	cmd := oktaAwsCLICmd(ctx, args...)
+	cmd.Stdout = os.Stdout
+	// Always set up the Okta environment variables based off of what's
+	// in the box config, regardless of what's currently in the environment.
+	cmd.Env = cmd.Environ()
+	cmd.Env = append(
+		cmd.Env,
+		"OKTA_ORG_DOMAIN="+b.AWS.Okta.OrgDomain,
+		"OKTA_AWS_ACCOUNT_FEDERATION_APP_ID="+b.AWS.Okta.FederationAppID,
+		"OKTA_OIDC_CLIENT_ID="+b.AWS.Okta.OIDCClientID,
+	)
 	return cmd.Run()
+}
+
+// isOktaAwsCliVersion1 determines what major version of okta-aws-cli
+// is installed. If the CLI is not installed and in dry run mode,
+// return false (not v1).
+func isOktaAwsCliVersion1(ctx context.Context, cliExists bool) (bool, error) {
+	if !cliExists {
+		// Assumes that we're in dry run mode
+		return false, nil
+	}
+
+	cmd := oktaAwsCLICmd(ctx, "--version")
+	output, err := cmd.Output()
+	if err != nil {
+		return false, errors.Wrap(err, "could not read version output")
+	}
+
+	return oktaAwsCliVersionOutputMatchesV1(output)
+}
+
+// oktaAwsCliVersionOutputMatchesV1 determines whether the
+// `okta-aws-cli --version` output is for major version 1.
+func oktaAwsCliVersionOutputMatchesV1(output []byte) (bool, error) {
+	matches := vRE.FindSubmatch(output)
+	if matches == nil {
+		return false, errors.Errorf("unknown version format: '%s'", output)
+	}
+
+	if len(matches) < 2 {
+		return false, errors.New("cannot find version in output")
+	}
+
+	majorVersion := matches[1]
+
+	return string(majorVersion) == "1", nil
+}
+
+// credentialProviderFormat is the value of `--format` that corresponds
+// to the credential provider format, depending on the okta-aws-cli
+// major version.
+func credentialProviderFormat(isCLIVersion1 bool) CredentialsOutput {
+	if isCLIVersion1 {
+		return OutputCredentialProviderV1
+	}
+
+	return OutputCredentialProvider
 }

pkg/cli/aws/aws_test.go
@@ -8,8 +8,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/getoutreach/gobox/pkg/box"
 	logtest "github.com/sirupsen/logrus/hooks/test"
 	"gotest.tools/v3/assert"
+	"gotest.tools/v3/assert/cmp"
 )
 
 func Test_assumedToRole(t *testing.T) {
@@ -125,17 +127,19 @@ func Test_refreshCredsViaOktaAWSCLI(t *testing.T) {
 		copts := DefaultCredentialOptions()
 		copts.Log = log
 
+		b := &box.Config{}
+
 		acopts := &AuthorizeCredentialsOptions{
 			DryRun: true,
 		}
 
-		err := refreshCredsViaOktaAWSCLI(context.Background(), copts, acopts, "")
+		err := refreshCredsViaOktaAWSCLI(context.Background(), copts, acopts, b, "")
 		assert.NilError(t, err)
 		assert.Equal(t, len(hook.Entries), 2)
 		msg := hook.LastEntry().Message
 		assert.Assert(t, strings.HasPrefix(msg, "Dry Run: okta-aws-cli"))
-		assert.Assert(t, strings.Contains(msg, "--aws-iam-role "))
-		assert.Assert(t, strings.Contains(msg, "--write-aws-credentials"))
+		assert.Assert(t, cmp.Contains(msg, "--aws-iam-role "))
+		assert.Assert(t, cmp.Contains(msg, "--write-aws-credentials"))
 	})
 
 	t.Run("interactive role selection", func(t *testing.T) {
@@ -144,11 +148,13 @@ func Test_refreshCredsViaOktaAWSCLI(t *testing.T) {
 		copts.Role = ""
 		copts.Log = log
 
+		b := &box.Config{}
+
 		acopts := &AuthorizeCredentialsOptions{
 			DryRun: true,
 		}
 
-		err := refreshCredsViaOktaAWSCLI(context.Background(), copts, acopts, "")
+		err := refreshCredsViaOktaAWSCLI(context.Background(), copts, acopts, b, "")
 		assert.NilError(t, err)
 		assert.Equal(t, len(hook.Entries), 2)
 		msg := hook.LastEntry().Message
@@ -161,17 +167,52 @@ func Test_refreshCredsViaOktaAWSCLI(t *testing.T) {
 		copts := DefaultCredentialOptions()
 		copts.Log = log
 
+		b := &box.Config{}
+
 		acopts := &AuthorizeCredentialsOptions{
 			DryRun: true,
 			Output: OutputCredentialProvider,
 		}
 
-		err := refreshCredsViaOktaAWSCLI(context.Background(), copts, acopts, "")
+		err := refreshCredsViaOktaAWSCLI(context.Background(), copts, acopts, b, "")
 		assert.NilError(t, err)
-		assert.Equal(t, len(hook.Entries), 2)
+		expectedEntryCount := 2
+		if os.Getenv("CI") != "" {
+			// extra log for missing okta-aws-cli since it's not installed by CI
+			expectedEntryCount = 3
+		}
+		assert.Equal(t, len(hook.Entries), expectedEntryCount)
 		msg := hook.LastEntry().Message
 		assert.Assert(t, strings.HasPrefix(msg, "Dry Run: okta-aws-cli"))
 		assert.Assert(t, !strings.Contains(msg, "--write-aws-credentials"))
-		assert.Assert(t, strings.Contains(msg, "--format credential-provider"))
+		assert.Assert(t, cmp.Contains(msg, "--format process-credentials"))
 	})
+}
+
+func Test_oktaAwsCliVersionOutputMatchesV1(t *testing.T) {
+	isV1, err := oktaAwsCliVersionOutputMatchesV1([]byte("\nokta-aws-cli version 1.2.1\n"))
+	assert.NilError(t, err)
+	assert.Assert(t, isV1)
+}
+
+func Test_oktaAwsCliVersionOutputDoesntMatchV2Beta(t *testing.T) {
+	isV1, err := oktaAwsCliVersionOutputMatchesV1([]byte("\nokta-aws-cli version 2.0.0-beta.5\n"))
+	assert.NilError(t, err)
+	assert.Assert(t, !isV1)
+}
+
+func Test_oktaAwsCliVersionOutputDoesntMatchV10(t *testing.T) {
+	isV1, err := oktaAwsCliVersionOutputMatchesV1([]byte("\nokta-aws-cli version 10.2.3\n"))
+	assert.NilError(t, err)
+	assert.Assert(t, !isV1)
+}
+
+func Test_oktaAwsCliVersionOutputErrorsWithUnknownOutput(t *testing.T) {
+	_, err := oktaAwsCliVersionOutputMatchesV1([]byte("\nError: unknown flag: --version\n"))
+	assert.ErrorContains(t, err, "unknown version format")
+}
+
+func Test_credentialProviderFormat(t *testing.T) {
+	assert.Equal(t, credentialProviderFormat(true), OutputCredentialProviderV1)
+	assert.Equal(t, credentialProviderFormat(false), OutputCredentialProvider)
 }