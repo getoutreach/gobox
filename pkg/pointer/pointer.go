320f5ea5b459122c9aa1cbd968df060acbf49e02
romain-tadiello <romain.tadiello@outreach.io>
2023-11-16 17:06:38 +0100

    adding functions comments

--- pkg/pointer/pointer.go
+++ pkg/pointer/pointer.go
@@ -11,10 +11,12 @@
 // with map / slice
 package pointer
 
+// ToPtr returns a pointer to {object} of type *T
 func ToPtr[T any](object T) *T {
 	return &object
 }
 
+// ToSlicePtr returns a slice of pointers of type *T pointing to each element of {objects}
 func ToSlicePtr[T any](objects []T) []*T {
 	ptrObj := make([]*T, 0, len(objects))
 	for _, o := range objects {
@@ -23,6 +25,7 @@ func ToSlicePtr[T any](objects []T) []*T {
 	return ptrObj
 }
 
+// ToMapPtr returns a map of pointers of type *T pointing to each element of {objectMap}
 func ToMapPtr[K comparable, T any](objectMap map[K]T) map[K]*T {
 	ptrObj := make(map[K]*T, len(objectMap))
 	for k, o := range objectMap {
@@ -31,6 +34,8 @@ func ToMapPtr[K comparable, T any](objectMap map[K]T) map[K]*T {
 	return ptrObj
 }
 
+// ToValue returns the value of type T pointed by {ptr}
+// if {ptr} is nil, return 0 value of T
 func ToValue[T any](ptr *T) (res T) {
 	if ptr == nil {
 		return res
@@ -38,6 +43,8 @@ func ToValue[T any](ptr *T) (res T) {
 	return *ptr
 }
 
+// ToSliceValue returns a slice of values of type T pointed by each pointers in {ptrs}
+// if a pointer in {ptrs} is nil the value at the corresponding index will be the 0 of T
 func ToSliceValue[T any](ptrs []*T) []T {
 	res := make([]T, 0, len(ptrs))
 	for _, ptr := range ptrs {
@@ -46,6 +53,8 @@ func ToSliceValue[T any](ptrs []*T) []T {
 	return res
 }
 
+// ToSliceValue returns a map of values of type T pointed by each pointers in {ptrs}
+// if a pointer in {ptrs} is nil the value at the corresponding key will be the 0 of T
 func ToMapValue[K comparable, T any](ptrs map[K]*T) map[K]T {
 	res := make(map[K]T, len(ptrs))
 	for v, ptr := range ptrs {
