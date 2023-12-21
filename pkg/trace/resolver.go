8431ffbcf0df70c55a8783c4ec6dde4b84447f1c
Petr Šmejkal <pesme@users.noreply.github.com>
2023-12-21 16:10:21 +0100

    lint

--- pkg/trace/resolver.go
+++ pkg/trace/resolver.go
@@ -1,6 +1,6 @@
 // Copyright 2023 Outreach Corporation. All Rights Reserved.
 
-// Description: This file contains
+// Description: This file contains constants and tools for controlling trace.Start/EndCall logging
 
 package trace
 
@@ -13,18 +13,19 @@ import (
 type InfoLoggingResolved int32
 
 const (
-	InfoLogging_Default  InfoLoggingResolved = 0
-	InfoLogging_Enabled  InfoLoggingResolved = 1
-	InfoLogging_Disabled InfoLoggingResolved = 2
+	InfoLoggingDefault  InfoLoggingResolved = 0
+	InfoLoggingEnabled  InfoLoggingResolved = 1
+	InfoLoggingDisabled InfoLoggingResolved = 2
 )
 
 type InfoLoggingResolver = func(ctx context.Context, operation string) InfoLoggingResolved
 
+// ResolvedLogging returns signals trace.EndCall whether to enable/disable info logging
 func ResolvedLogging(logging InfoLoggingResolved) log.Marshaler {
-	if logging == InfoLogging_Default {
+	if logging == InfoLoggingDefault {
 		return nil
 	}
-	if logging == InfoLogging_Enabled {
+	if logging == InfoLoggingEnabled {
 		return WithInfoLoggingDisabled()
 	}
 	return WithInfoLoggingDisabled()
