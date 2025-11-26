5adbe37f892d2ca14c23ba65acf2a6210afe7dec
MatyasVondraOutreach <matyas.vondra@outreach.io>
2025-11-26 14:17:00 +0100

    test(orerr): add tests for Is method

--- pkg/orerr/statuscodewrap_test.go
+++ pkg/orerr/statuscodewrap_test.go
@@ -12,16 +12,27 @@ import (
 	"github.com/getoutreach/gobox/pkg/statuscodes"
 )
 
-func (suite) Basics(t *testing.T) {
+func (suite) TestBasics(t *testing.T) {
 	erro := errors.New("bad")
 	err := orerr.NewErrorStatus(erro, statuscodes.Forbidden)
 
 	//nolint:errorlint // Why: test
 	assert.Equal(t, err.(*orerr.StatusCodeWrapper).StatusCode(), statuscodes.Forbidden)
 	//nolint:errorlint // Why: test
-	assert.Equal(t, err.(*orerr.StatusCodeWrapper).StatusCategory(), statuscodes.CategoryServerError)
+	assert.Equal(t, err.(*orerr.StatusCodeWrapper).StatusCategory(), statuscodes.CategoryClientError)
 	assert.Assert(t, orerr.IsErrorStatusCode(err, statuscodes.Forbidden))
 	assert.Assert(t, orerr.IsErrorStatusCategory(err, statuscodes.CategoryClientError))
 	assert.Assert(t, !orerr.IsErrorStatusCategory(err, statuscodes.CategoryServerError))
 	assert.Assert(t, !orerr.IsErrorStatusCategory(err, statuscodes.CategoryOK))
+}
+
+func (suite) TestIs(t *testing.T) {
+	unwrappedError := errors.New("bad")
+	wrappedError := orerr.NewErrorStatus(unwrappedError, statuscodes.Forbidden)
+	doubleWrappedError := errors.Wrap(wrappedError, "bad 2")
+
+	assert.Assert(t, errors.Is(wrappedError, &orerr.StatusCodeWrapper{}))
+	assert.Assert(t, errors.Is(doubleWrappedError, &orerr.StatusCodeWrapper{}))
+	assert.Assert(t, !errors.Is(unwrappedError, &orerr.StatusCodeWrapper{}))
+	assert.Assert(t, !errors.Is(nil, &orerr.StatusCodeWrapper{}))
 }
